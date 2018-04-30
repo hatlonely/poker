865828c1c89a161d387847b5c7a424ca90710385
hatlonely <hatlonely@gmail.com>
2018-04-30 21:45:26 +0800

    add resource

---
+++ resource/playingarts/README.md
@@ -0,0 +1,4 @@
+## Playing arts
+
+网址: <https://playingarts.com/en>
+代码: `$.each($("img.ng-isolate-scope.loaded"), function(i,v){console.log(v.src)})`
---
+++ resource/playingarts/two/two.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+require 'uri'
+require "open-uri"
+
+File.open("two.txt", "r") do |file|
+    file.each_line do |line|
+        name = URI.parse(line).path.split('/').last
+        puts line
+        open(line) {|data|
+            File.open(name, "wb") do |jpg|
+              jpg.puts data.read
+            end
+         }
+    end
+end
